9bb486c2ca4a4f562b4a77d48a166ab28bac68a4
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-09-05 16:43:01 +0200

    printing deadlines/no deadline dep on value

--- src/showDay/ShowDay.js
+++ src/showDay/ShowDay.js
@@ -1,17 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import moment from 'moment';
+
 
 function ShowDay({value, toDo}) {
 
   const date = value.clone()._d.toString().slice(0, 10)
+  let result = toDo.find((items) => items.date === date);
+  console.log('result', result);
 
   return (
     <div className="ShowDay">
       <h2>{date}</h2>
       <ul>
-        {toDo.map((item) => 
-          (item.date === date) && item.task.map((task, index) => 
-          <li key={index}>{task}</li>))}
+        {(result !== undefined) ? 
+          toDo.map((item) => 
+            (item.date === date) && item.task.map((task, index) => 
+              <li key={index}>{task}</li> )) 
+              : <li>No deadlines today!</li>}
       </ul>
     </div>
   );

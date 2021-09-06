7a905383b7af500e8ee58a7d108e0d0b52140600
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-09-06 10:05:07 +0200

    added task id and show item if checked is false. Not updated checked state yet

--- src/showDay/ShowDay.js
+++ src/showDay/ShowDay.js
@@ -6,15 +6,28 @@ function ShowDay({value, toDo}) {
   const date = value.clone()._d.toString().slice(0, 10)
   let result = toDo.find((item) => item.date === date);
 
+  const handleOnChange = (evt) => {
+
+    if (evt.target.checked) {
+      console.log('true');
+    }
+
+  }
+
+
   return (
     <div className="ShowDay">
       <h2>{date}</h2>
       <ul>
         {(result !== undefined) ? 
           toDo.map((item) => 
-            (item.date === date) && item.item.map((item, index) => 
-              <li key={index}>{item.task}<input type="checkbox" onChange={console.log('change to checked')}/></li> )) 
-              : <li>No deadlines today!</li>}
+            (item.date === date) && item.item.map((item) => 
+            ((item.checked === false) && 
+              <li key={item.id}>{item.task}
+                <input key={item.id} type="checkbox" onChange={handleOnChange}/>
+              </li>)              
+            )) 
+          : <li>No deadlines today!</li>} 
       </ul>
     </div>
   )
